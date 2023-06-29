1ac6ebfa9b7faf937d3a87b49ea21c0fa99c0f4e
realsanjeev <realsanjeev2@gmail.com>
2023-06-30 00:46:43 +0545

    including media file

--- travel_site/travel_site/settings.py
+++ travel_site/travel_site/settings.py
@@ -125,3 +125,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+
+# to add media directory
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
