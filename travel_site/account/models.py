ad8b6444ebd3251afd86ad61ab3a2bc04e5a7a46
realsanjeev <realsanjeev2@gmail.com>
2023-09-07 18:43:53 +0545

    adding profile page prototype

--- travel_site/account/models.py
+++ travel_site/account/models.py
@@ -9,7 +9,8 @@ class Profile(models.Model):
         upload_to="profile_images",
         default="blank-profile-photo.jpeg")
     location = models.CharField(max_length=100, blank=True)
-    dob = models.DateField(null=True)
+    dob = models.DateField(null=True, blank=False)
+    bio = models.TextField(null=True, blank=True)
 
     def __str__(self):
-        return self.user
+        return str(self.user)  # Return the username of the associated user
--- travel_site/account/views.py
+++ travel_site/account/views.py
@@ -47,34 +47,42 @@ def logout_user(request):
 
 def register_user(request):
     signup_template = os.path.join("account", "signup.html")
+    if request.user.is_authenticated:
+        return redirect('/home')
     if request.method == "POST":
         display_in_console("Register-user")
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password')
         confirm_password = request.POST.get('confirmpassword')
-        display_in_console("$" * 88)
+        display_in_console(f"{username} -- {email} -- {password}")
 
         if password == confirm_password:
             # Create the user and perform additional registration logic as needed
             user = User.objects.create_user(username=username,
                                             email=email,
                                             password=password)
+            Profile.objects.create(user=user).save()
             login(request, user)
             # Redirect to the protected page after successful registration
             # return redirect("home")
-            return render(request, signup_template)
+            return render(request, "home.html")
         else:
             context = {"error_message": "Passwords do not match"}
             return render(request, signup_template, context)
     return render(request, signup_template)
 
 def account_view(request):
+    profile_template = os.path.join("account", "profile.html")
     # return HttpResponse("ok done")
     records = Profile.objects.all() 
+    record = Profile.objects.all().first()
+    display_in_console(record.__dict__)
+    context = {}
+    context["user"] = record
     print(records)
     # for record in records:
     #     print(record.__dict__)
     # pass
-    return HttpResponse("Account view")
+    return render(request, profile_template, context=context)
     
